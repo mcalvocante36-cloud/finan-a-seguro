ad60803c0c8eee35f10e6d8ff4e18141d316ece6
marcos tulio cavalcanti barbosa <mcalvocante36@gmail.com>
2025-11-21 20:54:14 -0300

    Corrigindo base path para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// No seu arquivo vite.config.js
 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
+  // Adicione ESTA linha:
+  base: '/finan-a-seguro/', 
   plugins: [react()],
-});
+})
