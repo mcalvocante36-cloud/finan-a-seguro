552629acf94b33ea7c3852aca019dd26e19ceaf4
marcos tulio cavalcanti barbosa <mcalvocante36@gmail.com>
2025-11-21 21:20:23 -0300

    Corrigindo root do Vite para usar a pasta docs

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Adicione ESTA linha:
+  // ESTA linha diz ao Vite onde encontrar o index.html
+  root: 'docs', 
+
+  // ESTA linha já estava correta para o caminho do GitHub Pages
   base: '/finan-a-seguro/', 
+
   plugins: [react()],
 })
